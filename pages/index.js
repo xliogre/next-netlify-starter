baa3f3bb8d0897c59c2b471e74181f1aa95d3c17
xliogre <xli@ogresystems.com>
2022-10-19 11:02:30 -0500

    Add Charting library
    Add Charting library

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+          <title>TradingView Charting Library demo</title>
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"></meta>
+          <script type="text/javascript" src="charting_library/charting_library.standalone.js"></script>
+		      <script type="text/javascript" src="datafeeds/udf/dist/bundle.js"></script>
       </Head>
 
       <main>
